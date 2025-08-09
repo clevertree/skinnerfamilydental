7cd0022140fc35eae1fcadf0c7f5d6752ed1d005
Ari Asulin <ari.asulin@gmail.com>
2025-08-09 17:29:58 -0400

    fix front page slideshow

--- src/app/service/dental-hygiene/page.tsx
+++ src/app/service/dental-hygiene/page.tsx
@@ -1,52 +1,54 @@
 import React from 'react';
-import PhilipsAd from "@/app/frag/philipsAd";
+import PhilipsAd from "@app/frag/philipsAd";
 
 export default function DentalHygienePage() {
-  return (
-    <div>
-      {/* TODO: Add converted content from service/dental-hygiene.html here */}
-      <div id="r5002" role="main" className="row designRow">
-        <div id='p6314' className="container container-fluid">
-          <div className="col col-sm-12">
-            <div id="r2801" className="row bgfilter">
-              <div className="container container-fixed">
-                <div id="c2555" className="col col col-sm-1 hidden-sm"></div>
-                <div id="c3294" className="col col-lg-10 col-md-10 col-sm-12">
-                  <div id="m3261" className="module text">
-                    <h1 className="spottext" style={{ textAlign: 'center' }}>Dental Hygiene</h1>
-                  </div>
+    return (
+        <div>
+            {/* TODO: Add converted content from service/dental-hygiene.html here */}
+            <div id="r5002" role="main" className="row designRow">
+                <div id='p6314' className="container container-fluid">
+                    <div className="col col-sm-12">
+                        <div id="r2801" className="row bgfilter">
+                            <div className="container container-fixed">
+                                <div id="c2555" className="col col col-sm-1 hidden-sm"></div>
+                                <div id="c3294" className="col col-lg-10 col-md-10 col-sm-12">
+                                    <div id="m3261" className="module text">
+                                        <h1 className="spottext" style={{textAlign: 'center'}}>Dental Hygiene</h1>
+                                    </div>
+                                </div>
+                                <div id="c4032" className="col col col-sm-1 hidden-sm"></div>
+                            </div>
+                        </div>
+                        <div id="r1870" className="row ">
+                            <div className="container container-fixed">
+                                <div className="col col-sm-12 col-lg-2 col-md-1 hidden-sm flexCol">
+                                    <div id="m4464" className="module autospacer"></div>
+                                </div>
+                                <div id="c8354" className="col col-sm-12 col-lg-8 col-md-10">
+                                    <div id="m4556" className="module text">
+                                        <h2 className="headline" style={{textAlign: 'center'}}>Dental Hygiene</h2>
+                                    </div>
+                                    <div id="m4726" className="module text">
+                                        <p className="bodytext" style={{textAlign: 'center'}}>
+                                            Routine prophylaxis appointments are available for the whole family.
+                                            Treatment planning and maintenance options are reviewed at every cleaning to
+                                            keep you up to date on all your dental needs, and help keep you smiling!
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col col-sm-12 col-lg-2 col-md-1 hidden-sm flexCol">
+                                    <div id="m2999" className="module autospacer"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="r4222" className="row ">
+                            <div className="container container-fixed">
+                                <PhilipsAd/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div id="c4032" className="col col col-sm-1 hidden-sm"></div>
-              </div>
             </div>
-            <div id="r1870" className="row ">
-              <div className="container container-fixed">
-                <div className="col col-sm-12 col-lg-2 col-md-1 hidden-sm flexCol">
-                  <div id="m4464" className="module autospacer"></div>
-                </div>
-                <div id="c8354" className="col col-sm-12 col-lg-8 col-md-10">
-                  <div id="m4556" className="module text">
-                    <h2 className="headline" style={{ textAlign: 'center' }}>Dental Hygiene</h2>
-                  </div>
-                  <div id="m4726" className="module text">
-                    <p className="bodytext" style={{ textAlign: 'center' }}>
-                      Routine prophylaxis appointments are available for the whole family. Treatment planning and maintenance options are reviewed at every cleaning to keep you up to date on all your dental needs, and help keep you smiling!
-                    </p>
-                  </div>
-                </div>
-                <div className="col col-sm-12 col-lg-2 col-md-1 hidden-sm flexCol">
-                  <div id="m2999" className="module autospacer"></div>
-                </div>
-              </div>
-            </div>
-            <div id="r4222" className="row ">
-              <div className="container container-fixed">
-                <PhilipsAd/>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/app/service/sedation-dentistry/page.tsx
+++ src/app/service/sedation-dentistry/page.tsx
@@ -1,93 +1,93 @@
 import React from 'react';
-import PhilipsAd from "@/app/frag/philipsAd";
+import PhilipsAd from "@app/frag/philipsAd";
 
 export default function SedationDentistryPage() {
-  return (
-      <div id="r5002" role="main" className="row designRow">
-          <div id='p5774' className="container container-fluid">
-              <div className="col col-sm-12">
-                  <div id="r2801" className="row bgfilter">
-                      <div className="container container-fixed">
-                          <div id="c2555" className="col col col-sm-1 hidden-sm" />
-                          <div id="c3294" className="col col-lg-10 col-md-10 col-sm-12">
-                              <div id="m3261" className="module text">
-                                  <h1 className="spottext" style={{ textAlign: "center" }}>
-                                      Sedation Dentistry
-                                  </h1>
-                              </div>
-                          </div>
-                          <div id="c4032" className="col col col-sm-1 hidden-sm" />
-                      </div>
-                  </div>
-                  <div id="r1643" className="row ">
-                      <div className="container container-fixed colsStack">
-                          <div className="col col-sm-12 col-md-1 hidden-sm hidden-md flexCol">
-                              <div id="m4480" className="module autospacer" />
-                          </div>
-                          <div id="c2222" className="col col-sm-12 col-lg-5 col-md-6">
-                              <div id="m2227" className="module text">
-                                  <h2 className="headline" style={{ textAlign: "left" }}>
-                                      Sedation Dentistry
-                                  </h2>
-                              </div>
-                              <div id="m3339" className="module text">
-                                  <p className="bodytext" style={{ textAlign: "left" }}>
-                                      At our office we would like our patients to be as comfortable as
-                                      possible. We offer everything from blankets and television to
-                                      various sedation options for your optimal comfort level. At your
-                                      request, we can review which option is best for you and your
-                                      health at your hygiene/treatment planning appointment. Options
-                                      currently include: Nitrous Oxide (laughing gas) and I.V.
-                                      Sedation. *Sedative medications may also be prescribed before
-                                      your appointment.
-                                  </p>
-                                  <p className="bodytext" style={{ textAlign: "left" }}>
-                                      &nbsp;
-                                  </p>
-                                  <p className="bodytext" style={{ textAlign: "left" }}>
-                                      Private appointments can be scheduled for sedation services.
-                                  </p>
-                              </div>
-                          </div>
-                          <div id="c3191" className="col col-sm-12 col-lg-5 col-md-6">
-                              <div id="m1906" className="module text">
-                                  <h3 className="subtitle" style={{ textAlign: "left" }}>
-                                      &nbsp;Learn More About IV Sedation
-                                  </h3>
-                              </div>
-                              <div id="m3700" className="module video " data-req="video">
-                                  <div className="cookieconsent-optin-marketing mono-video-wrapper">
-                                      <div
-                                          className="fluid-width-video-wrapper"
-                                      >
-                                          <iframe
-                                              id="player_m3700"
-                                              title="IV Sedation - Q & A - General Anesthesia"
-                                              data-videotype="youtube"
-                                              data-req="ext-//www.youtube.com/iframe_api"
-                                              src="https://www.youtube.com/embed/u5FWx0B41rE?enablejsapi=1&playerapiid=player_m3700"
-                                              style={{ border: 0 }}
-                                              allowFullScreen
-                                              name="fitvid0"
-                                          />
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                          <div className="col col-sm-12 col-md-1 hidden-sm hidden-md flexCol">
-                              <div id="m1595" className="module autospacer" />
-                          </div>
-                      </div>
-                  </div>
-                  <div id="r4222" className="row ">
-                      <div className="container container-fixed">
-                          <PhilipsAd/>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
+    return (
+        <div id="r5002" role="main" className="row designRow">
+            <div id='p5774' className="container container-fluid">
+                <div className="col col-sm-12">
+                    <div id="r2801" className="row bgfilter">
+                        <div className="container container-fixed">
+                            <div id="c2555" className="col col col-sm-1 hidden-sm"/>
+                            <div id="c3294" className="col col-lg-10 col-md-10 col-sm-12">
+                                <div id="m3261" className="module text">
+                                    <h1 className="spottext" style={{textAlign: "center"}}>
+                                        Sedation Dentistry
+                                    </h1>
+                                </div>
+                            </div>
+                            <div id="c4032" className="col col col-sm-1 hidden-sm"/>
+                        </div>
+                    </div>
+                    <div id="r1643" className="row ">
+                        <div className="container container-fixed colsStack">
+                            <div className="col col-sm-12 col-md-1 hidden-sm hidden-md flexCol">
+                                <div id="m4480" className="module autospacer"/>
+                            </div>
+                            <div id="c2222" className="col col-sm-12 col-lg-5 col-md-6">
+                                <div id="m2227" className="module text">
+                                    <h2 className="headline" style={{textAlign: "left"}}>
+                                        Sedation Dentistry
+                                    </h2>
+                                </div>
+                                <div id="m3339" className="module text">
+                                    <p className="bodytext" style={{textAlign: "left"}}>
+                                        At our office we would like our patients to be as comfortable as
+                                        possible. We offer everything from blankets and television to
+                                        various sedation options for your optimal comfort level. At your
+                                        request, we can review which option is best for you and your
+                                        health at your hygiene/treatment planning appointment. Options
+                                        currently include: Nitrous Oxide (laughing gas) and I.V.
+                                        Sedation. *Sedative medications may also be prescribed before
+                                        your appointment.
+                                    </p>
+                                    <p className="bodytext" style={{textAlign: "left"}}>
+                                        &nbsp;
+                                    </p>
+                                    <p className="bodytext" style={{textAlign: "left"}}>
+                                        Private appointments can be scheduled for sedation services.
+                                    </p>
+                                </div>
+                            </div>
+                            <div id="c3191" className="col col-sm-12 col-lg-5 col-md-6">
+                                <div id="m1906" className="module text">
+                                    <h3 className="subtitle" style={{textAlign: "left"}}>
+                                        &nbsp;Learn More About IV Sedation
+                                    </h3>
+                                </div>
+                                <div id="m3700" className="module video " data-req="video">
+                                    <div className="cookieconsent-optin-marketing mono-video-wrapper">
+                                        <div
+                                            className="fluid-width-video-wrapper"
+                                        >
+                                            <iframe
+                                                id="player_m3700"
+                                                title="IV Sedation - Q & A - General Anesthesia"
+                                                data-videotype="youtube"
+                                                data-req="ext-//www.youtube.com/iframe_api"
+                                                src="https://www.youtube.com/embed/u5FWx0B41rE?enablejsapi=1&playerapiid=player_m3700"
+                                                style={{border: 0}}
+                                                allowFullScreen
+                                                name="fitvid0"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-sm-12 col-md-1 hidden-sm hidden-md flexCol">
+                                <div id="m1595" className="module autospacer"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="r4222" className="row ">
+                        <div className="container container-fixed">
+                            <PhilipsAd/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-  );
+    );
 }
 

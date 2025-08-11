d576946135f6c8c5b21cccc12f82077e3cead502
Ari Asulin <Ari.Asulin+uopx@phoenix.edu>
2025-08-11 09:59:53 -0400

    fix front page styles #2

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,40 +1,29 @@
 import ContactForm from "@components/Forms/ContactForm";
 import {sendContactFormEmail} from "../actions/emailActions";
 import React from "react";
+import {Paper} from "@mui/material";
 
 export function ContactSection() {
     return <div
         id="r3893"
         className="row "
-        data-req="parallax,parallax,parallax,parallax,parallax,parallax,parallax,parallax,parallax,parallax,parallax,parallax,parallax,parallax"
         style={{
-            backgroundPosition: "100% 143.727px",
+            backgroundColor: '#c8e6ee',
+            backgroundPosition: "60% 143.727px",
             backgroundAttachment: "fixed"
         }}
     >
-        {" "}
-        <div className="container container-fixed colsStack">
-            <div id="c2542" className="col col-sm-12 col-lg-6 col-md-7 flexCol">
-                <div id="m1056" className="module autospacer"/>
+        <Paper className='m-8 p-8 max-w-[40rem]'>
+            <div className="flexWrap">
                 {" "}
-                <div className="flexWrap">
-                    {" "}
-                    <div id="m4594" className="module text">
-                        <p className="darkspottext" style={{textAlign: "left"}}>
-                            Send us a message
-                        </p>
-                    </div>
+                <div id="m4594" className="module text">
+                    <p className="darkspottext" style={{textAlign: "left"}}>
+                        Send us a message
+                    </p>
                 </div>
-                <ContactForm sendForm={sendContactFormEmail}/>
             </div>
-            {" "}
-            <div className="col col-sm-12 hidden-sm col-lg-5 col-md-4 flexCol">
-                <div id="m1509" className="module autospacer"/>
-                {" "}
-            </div>
-            {" "}
-        </div>
-        {" "}
+            <ContactForm sendForm={sendContactFormEmail}/>
+        </Paper>
     </div>
 
 }
